8575ee482a70bdde4585511df7cd8be9ebaa3c2b
olzhas0986 <hyuga4319@gmail.com>
2025-03-28 14:53:28 +0500

    Create p.sh

---
+++ p.sh
@@ -0,0 +1,18 @@
+echo "Installing necessary packages."
+sudo apt update 
+sudo apt upgrade 
+sudo apt install -y flex 
+sudo apt install -y git 
+sudo apt install -y bc 
+sudo apt install -y bison 
+sudo apt install -y python-is-python3
+sudo apt install -y gcc
+sudo apt install -y ccache
+sudo apt-get install -y gcc-aarch64-linux-gnu
+sudo apt install -y cpio
+sudo apt install -y python3
+sudo apt install -y python2
+sudo apt install -y p7zip-full
+sudo apt install -y lld
+sudo apt install -y wget
+echo "All essential packages has been installed."
